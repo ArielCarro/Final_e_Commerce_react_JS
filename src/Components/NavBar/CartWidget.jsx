18cfedae6de5a39e6243cc80958f9c8b670f8fda
Ariel Carro <carroariel@gmail.com>
2022-07-05 17:21:23 -0300

    Se hace primer commit

---
+++ src/Components/NavBar/CartWidget.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import logo from '../../img/react-bootstrap.svg'
+
+const CartWidget = () => {
+  return (
+    <div>
+        <img src={logo} className='cartWidget'/>
+    </div>
+  )
+}
+
+export default CartWidget
