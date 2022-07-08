62e49d9c24dbac448623ff23ae785ee17257eb36
Ariel Carro <carroariel@gmail.com>
2022-07-08 14:35:36 -0300

    Primer commit

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ function App() {
     <>
       <div className="App">
         <NavBar />
-        <ItemListContainer greeting="Soy un item list container" />
+        <ItemListContainer greeting="Este es un contador" />
       </div>
     </>
   );
--- src/Components/itemListContainer/ItemListContainer.jsx
+++ src/Components/itemListContainer/ItemListContainer.jsx
@@ -1,8 +1,17 @@
 import React from 'react'
+import ItemCount from './ItemCount'
 
 const ItemListContainer = ({greeting}) => {
+
+  const onAdd = (count) =>{
+    alert (`Se han añadido ${count} productos al carrito`)
+  }
+  
   return (
+    <>
     <div className='greetingContainer'>{greeting}</div>
+    <ItemCount init={1} stock={5} onAdd={onAdd}/>
+    </>
   )
 }
 
